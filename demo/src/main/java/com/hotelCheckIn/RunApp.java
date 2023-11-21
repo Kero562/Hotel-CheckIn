61dc74d5041ce69a814932af8609331ce392e460
Altoid0 <tanay.v.shah@gmail.com>
2023-11-20 22:19:18 -0800

    db with more mock data

--- demo/src/main/java/com/hotelCheckIn/RunApp.java
+++ demo/src/main/java/com/hotelCheckIn/RunApp.java
@@ -6,7 +6,7 @@ public class RunApp {
         DatabaseUtil dbManager = new DatabaseUtil();
         dbManager.initializeDB();
 
-        // Current Customer ID/UUID: 37028509
+        // // Current Customer ID/UUID: 43333863
         // int customerId = dbManager.addCustomer("John", "Doe", "1234567890", "John.Doe@gmail.com");
         // if (customerId == -1) {
         //     System.out.println("Customer not added skipping room and reservation.");
@@ -22,12 +22,44 @@ public class RunApp {
         //     }
         // }
 
+        // // 67311723
+        // customerId = dbManager.addCustomer("Jenny", "Sanders", "8587986187", "jenny@gmail.com");
+        // if (customerId == -1) {
+        //     System.out.println("Customer not added skipping room and reservation.");
+        // } else {
+        //     int roomNumber = dbManager.addRoom(102, 4, 200.00, "DoubleRoom", "Empty");
+        //     if (roomNumber == -1) {
+        //         System.out.println("Room not added skipping reservation.");
+        //     } else {
+        //         // Maybe create a view for this
+        //         // https://stackoverflow.com/questions/8753959/round-a-floating-point-number-to-the-next-integer-value-in-java
+        //         // totalCost = (int) Math.ceil((checkout-checkin)/3600) * dailyRate
+        //         dbManager.addReservation(customerId, roomNumber, 1698867509047L, 1704013122000L, "Pending");
+        //     }
+        // }
+
+        // // 16338193
+        // customerId = dbManager.addCustomer("Jacob", "Smith", "6195678934", "jsmith@gmail.com");
+        // if (customerId == -1) {
+        //     System.out.println("Customer not added skipping room and reservation.");
+        // } else {
+        //     int roomNumber = dbManager.addRoom(103, 4, 200.00, "DoubleRoom", "Empty");
+        //     if (roomNumber == -1) {
+        //         System.out.println("Room not added skipping reservation.");
+        //     } else {
+        //         // Maybe create a view for this
+        //         // https://stackoverflow.com/questions/8753959/round-a-floating-point-number-to-the-next-integer-value-in-java
+        //         // totalCost = (int) Math.ceil((checkout-checkin)/3600) * dailyRate
+        //         dbManager.addReservation(customerId, roomNumber, 1698867509047L, 1704013122000L, "Pending");
+        //     }
+        // }
+
         // dbManager.addAdmin("admin", "password");
         
-        String logView = "CREATE VIEW IF NOT EXISTS log AS\n"
-        + "SELECT c.customer_id, c.last_name, c.email, r.room_number, r.reservation_status\n"
-        + "FROM customer c, reservation r\n"
-        + "INNER JOIN reservation ON c.customer_id = r.customer_id;";
+        String logView = "CREATE VIEW IF NOT EXISTS log AS "
+        + "SELECT c.customer_id, c.last_name, c.email, r.room_number, r.reservation_status "
+        + "FROM customer c "
+        + "INNER JOIN reservation r ON c.customer_id = r.customer_id;";
         dbManager.executeStatement(logView);
 
         String serviceLog = "CREATE VIEW IF NOT EXISTS service_log AS\n"
